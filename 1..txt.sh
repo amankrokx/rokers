68a57f65eea6f770da39ff49511c14c6ebd04992
Aman KUmar <amankumar.spj410@gmail.com>
2023-03-07 00:46:47 +0530

    sfsdvgdf

--- 1..txt.sh
+++ 1..txt.sh
@@ -2,4 +2,9 @@ sudo nano /etc/rc.local
 alsamixer
 omxplayer '/home/pi/Tere Naina.wav' -o alsa:hw:1
 
---rc-host localhost:port // 8088
+ssh pi@192.168.137.163
+// password - raspberry
+
+vlc --rc-host 127.0.0.1:8088 --alsa-audio-device hw:1,0
+cd rokers
+node app.js
